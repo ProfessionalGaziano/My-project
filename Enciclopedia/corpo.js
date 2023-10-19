e5b85688ec65be9a5fd4e87fb87e4086f907d016
Francesco Gaziano <141562843+ProfessionalGaziano@users.noreply.github.com>
2023-10-19 17:30:22 +0200

    Add files via upload

---
+++ Enciclopedia/corpo.js
@@ -0,0 +1,32 @@
+let img=document.querySelector('img');
+let btn1=document.querySelector('#btn1');
+let btn2=document.querySelector('#btn2');
+let btn3=document.querySelector('#btn3');
+let btn4=document.querySelector('#btn4');
+let btn5=document.querySelector('#btn5');
+
+
+btn1.addEventListener('click',()=>{
+  img.src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Nikola_Tesla_by_Sarony_c1885-crop.png/220px-Nikola_Tesla_by_Sarony_c1885-crop.png';
+
+})
+
+btn2.addEventListener('click',()=>{
+  img.src='https://staticfanpage.akamaized.net/wp-content/uploads/sites/5/2017/03/terra-cover.jpg';
+
+})
+
+btn3.addEventListener('click',()=>{
+  img.src='https://images.everyeye.it/img-notizie/alcune-parti-matematica-teorica-moderna-potrebbero-crollare-v3-402141-600x320.jpg';
+
+})
+
+btn4.addEventListener('click',()=>{
+  img.src='https://sites.google.com/site/corsoradiostereo1960/_/rsrc/1252331047795/corso-radio-stereo-teorica-30-/Fig01.jpg';
+
+})
+
+btn5.addEventListener('click',()=>{
+  img.src='https://www.wordsmart.it/wp-content/uploads/2018/11/miglior-monitor-4K-HDR.jpg';
+
+})
